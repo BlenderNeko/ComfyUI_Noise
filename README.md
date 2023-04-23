369a03c8c473a173d818a90681b9e20b313eb7be
BlenderNeko <126974546+BlenderNeko@users.noreply.github.com>
2023-04-23 22:17:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,82 @@
+# Advanced CLIP Text Encode
+
+This repo contains 6 nodes for [ComfyUI](https://github.com/comfyanonymous/ComfyUI) that allows for more control and flexibility over the noise. This allows e.g. for workflows with small variations to generations.
+
+## Nodes
+
+### Noisy Latent Image:
+This node lets you generate noise and has the following settings:
+- **source**: where to generate the noise, currently supports GPU and CPU.
+- **seed**: the noise seed.
+- **width**: image width.
+- **height**: image height.
+- **batch_size**: batch size.
+
+### Duplicate Batch Index:
+This node lets you duplicate a certain sample in the batch, this can be used to duplicate e.g. encoded images but also noise generated from the node listed above. It has the following settings:
+- **latents**: the latents.
+- **batch_index**: which sample in the latents to duplicate.
+- **batch_size**: the new batch size, (i.e. how many times to duplicate the sample).
+
+### Slerp Latents:
+This node lets you mix two latents together. Both of the input latents must share the same dimensions or the node will ignore the mix factor and instead output the top slot. When it comes to other things attached to the latents such as e.g. masks, only those of the top slot are passed on. This node comes with the following inputs:
+- **latents1**: first batch of latents.
+- **latents2**: second batch of latents
+- **factor**: how much of the second batch of latents should be slerped into the first.
+
+### Get Sigma:
+This node can be used to calculate the amount of noise a sampler expects when it starts denoising. It comes with the following inputs and settings:
+- **model**: The model for which to calculate the sigma.
+- **sampler_name**: the name of the sampler for which to calculate the sigma.
+- **scheduler**: the type of schedule used in the sampler
+- **steps**: the total number of steps in the schedule
+- **start_at_step**: the start step of the sampler, i.e. how much noise it expects in the input image
+- **end_at_step**: the current end step of the previous sampler, i.e. how much noise already is in the image.
+
+Most of the time you'd simply want to keep `start_at_step` at zero, and `end_at_step` at `steps`, but if you'd want to re-inject some noise in between two samplers, e.g. one sampler that denoises from 0 to 15, and a second that denoises from 10 to 20, you'd want to use a `start_at_step` 10 and an `end_at_step` of 15. So that the image we get, which is at step 15, can be noised back down to step 10, so the second sampler can bring it to 20. Take note that the Advanced Ksampler has a settings for `add_noise` and `return_with_leftover_noise` which when working with these nodes we both want to have disabled.
+
+### Inject Noise:
+This node lets you actually inject the noise into an image latent, it comes with the following inputs:
+- **latents**: The latents to inject the noise into.
+- **noise**: The noise.
+- **strength**: The strength of the noise. Note that we can use the node above to calculate for us an appropriate strength value.
+
+### Unsampler:
+This node does the reverse of a sampler. It calculates the noise that would generate the image given the model and the prompt. It takes the following inputs and settings:
+- **model**: The model to target.
+- **steps**: number of steps to noise.
+- **cfg**: classifier free guidance scale.
+- **sampler_name**: The name of the sampling technique to use.
+- **scheduler**: The type of schedule to use.
+- **positive**: Positive prompt.
+- **negative**: Negative prompt.
+- **latent_image**: The image to renoise.
+
+When trying to reconstruct the target image as faithful as possible this works best if both the unsampler and sampler use a cfg scale close to 1.0 and similar number of steps. But it is fun and worth it to play around with these settings to get a better intuition of the results. This node let's you do similar things the A1111 [img2img alternative](https://github.com/AUTOMATIC1111/stable-diffusion-webui/wiki/Features#img2img-alternative-test) script does
+
+## Examples
+
+here are some examples that show how to use the nodes above. Workflows to these examples can be found in the `example_workflow` folder.
+
+<details>
+<summary>
+generating variations
+</summary>
+
+![screenshot of a workflow that demos generating small variations to a given seed](https://github.com/BlenderNeko/ComfyUI_noise/blob/master/examples/example_variation.png)
+
+To create small variations to a given generation we can do the following: We generate the noise of the seed that we're interested using a `Noisy Latent Image` node, we then create an entire batch of these with a `Duplicate Batch Index` node. Note that if we were doing this for img2img we can use this same node to duplicate the image latents. Next we generate some more noise, but this time we generate a batch of noise rather than a single sample. We then Slerp this newly created noise into the other one with a `Slerp Latents` node. To figure out the required strength for injecting this noise we use a `Get Sigma` node. And finally we inject the slerped noise into a batch of empty latents with a `Inject Noise` node. Take note that we use an advanced Ksampler with the `add_noise` setting disabled
+
+</details>
+
+<details>
+<summary>
+generating variations
+</summary>
+
+![screenshot of a workflow that demos generating small variations to a given seed](https://github.com/BlenderNeko/ComfyUI_noise/blob/master/examples/example_unsample.png)
+
+To get the noise that recreates a certain image, we first load an image. Then we use the `Unsampler` node with a low cfg value. To check if this is working we then take the resulting noise and feed it back into an advanced ksampler with the `add_noise` setting disabled, and a cfg of 1.0.
+
+</details>
+
---
+++ nodes.py
@@ -0,0 +1,219 @@
+import torch
+
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "comfy"))
+
+import comfy.model_management
+import comfy.sample
+
+MAX_RESOLUTION=8192
+
+class NoisyLatentImage:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {
+            "source":(["CPU", "GPU"], ),
+            "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
+            "width": ("INT", {"default": 512, "min": 64, "max": MAX_RESOLUTION, "step": 64}),
+            "height": ("INT", {"default": 512, "min": 64, "max": MAX_RESOLUTION, "step": 64}),
+            "batch_size": ("INT", {"default": 1, "min": 1, "max": 64}),
+            }}
+    RETURN_TYPES = ("LATENT",)
+    FUNCTION = "create_noisy_latents"
+
+    CATEGORY = "latent/noise"
+        
+    def create_noisy_latents(self, source, seed, width, height, batch_size):
+        torch.manual_seed(seed)
+        if source == "CPU":
+            device = "cpu"
+        else:
+            device = comfy.model_management.get_torch_device()
+        noise = torch.randn((batch_size,  4, height // 8, width // 8), dtype=torch.float32, device=device).cpu()
+        return ({"samples":noise}, )
+
+class DuplicateBatchIndex:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {
+            "latents":("LATENT",),
+            "batch_index": ("INT", {"default": 0, "min": 0, "max": 63}),
+            "batch_size": ("INT", {"default": 1, "min": 1, "max": 64}),
+            }}
+    
+    RETURN_TYPES = ("LATENT",)
+    FUNCTION = "duplicate_index"
+
+    CATEGORY = "latent"
+        
+    def duplicate_index(self, latents, batch_index, batch_size):
+        s = latents.copy()
+        batch_index = min(s["samples"].shape[0] - 1, batch_index)
+        target = s["samples"][batch_index:batch_index + 1].clone()
+        target = target.repeat((batch_size,1,1,1))
+        s["samples"] = target
+        return (s,)
+
+# from  https://discuss.pytorch.org/t/help-regarding-slerp-function-for-generative-model-sampling/32475
+def slerp(val, low, high):
+    dims = low.shape
+
+    #flatten to batches
+    low = low.reshape(dims[0], -1)
+    high = high.reshape(dims[0], -1)
+
+    low_norm = low/torch.norm(low, dim=1, keepdim=True)
+    high_norm = high/torch.norm(high, dim=1, keepdim=True)
+
+    # in case we divide by zero
+    low_norm[low_norm != low_norm] = 0.0
+    high_norm[high_norm != high_norm] = 0.0
+
+    omega = torch.acos((low_norm*high_norm).sum(1))
+    so = torch.sin(omega)
+    res = (torch.sin((1.0-val)*omega)/so).unsqueeze(1)*low + (torch.sin(val*omega)/so).unsqueeze(1) * high
+    return res.reshape(dims)
+
+class LatentSlerp:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {
+            "latents1":("LATENT",),
+            "latents2":("LATENT",),
+            "factor": ("FLOAT", {"default": .5, "min": 0.0, "max": 1.0, "step": 0.01}),
+            }}
+    
+    RETURN_TYPES = ("LATENT",)
+    FUNCTION = "slerp_latents"
+
+    CATEGORY = "latent"
+        
+    def slerp_latents(self, latents1, latents2, factor):
+        s = latents1.copy()
+        if latents1["samples"].shape != latents2["samples"].shape:
+            print("warning, shapes in LatentSlerp not the same, ignoring")
+            return (s,)
+        s["samples"] = slerp(factor, latents1["samples"].clone(), latents2["samples"].clone())
+        return (s,)
+
+class GetSigma:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {
+            "model": ("MODEL",),
+            "sampler_name": (comfy.samplers.KSampler.SAMPLERS, ),
+            "scheduler": (comfy.samplers.KSampler.SCHEDULERS, ),
+            "steps": ("INT", {"default": 10000, "min": 0, "max": 10000}),
+            "start_at_step": ("INT", {"default": 0, "min": 0, "max": 10000}),
+            "end_at_step": ("INT", {"default": 10000, "min": 0, "max": 10000}),
+            }}
+    
+    RETURN_TYPES = ("FLOAT",)
+    FUNCTION = "calc_sigma"
+
+    CATEGORY = "latent/noise"
+        
+    def calc_sigma(self, model, sampler_name, scheduler, steps, start_at_step, end_at_step):
+        end_at_step = min(steps, end_at_step)
+        start_at_step = min(start_at_step, end_at_step)
+        real_model = None
+        comfy.model_management.load_model_gpu(model)
+        real_model = model.model
+        sigmas = comfy.samplers.calculate_sigmas(real_model, steps, scheduler, sampler_name)
+        sigma = sigmas[start_at_step] - sigmas[end_at_step] 
+        print(sigma)
+        return (sigma.numpy(),)
+
+class InjectNoise:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {
+            "latents":("LATENT",),
+            "noise":  ("LATENT",),
+            "strength": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 20.0, "step": 0.01}),
+            }}
+    
+    RETURN_TYPES = ("LATENT",)
+    FUNCTION = "inject_noise"
+
+    CATEGORY = "latent/noise"
+        
+    def inject_noise(self, latents, noise, strength):
+        s = latents.copy()
+        if latents["samples"].shape != noise["samples"].shape:
+            print("warning, shapes in InjectNoise not the same, ignoring")
+            return (s,)
+        s["samples"] = s["samples"].clone() + noise["samples"].clone() * strength
+        return (s,)
+    
+class Unsampler:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required":
+                    {"model": ("MODEL",),
+                    "steps": ("INT", {"default": 20, "min": 1, "max": 10000}),
+                    "cfg": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 100.0}),
+                    "sampler_name": (comfy.samplers.KSampler.SAMPLERS, ),
+                    "scheduler": (comfy.samplers.KSampler.SCHEDULERS, ),
+                    "positive": ("CONDITIONING", ),
+                    "negative": ("CONDITIONING", ),
+                    "latent_image": ("LATENT", ),
+                    }}
+    
+    RETURN_TYPES = ("LATENT",)
+    FUNCTION = "unsampler"
+
+    CATEGORY = "sampling"
+        
+    def unsampler(self, model, cfg, sampler_name, steps, scheduler, positive, negative, latent_image):
+        device = comfy.model_management.get_torch_device()
+        latent = latent_image
+        latent_image = latent["samples"]
+        
+        noise = torch.zeros(latent_image.size(), dtype=latent_image.dtype, layout=latent_image.layout, device="cpu")
+        noise_mask = comfy.sample.create_mask(latent, noise)
+
+        real_model = None
+        comfy.model_management.load_model_gpu(model)
+        real_model = model.model
+
+        noise = noise.to(device)
+        latent_image = latent_image.to(device)
+
+        positive_copy = comfy.sample.broadcast_cond(positive, noise)
+        negative_copy = comfy.sample.broadcast_cond(negative, noise)
+
+        models = comfy.sample.load_additional_models(positive, negative)
+
+        sampler = comfy.samplers.KSampler(real_model, steps=steps, device=device, sampler=sampler_name, scheduler=scheduler, denoise=1.0, model_options=model.model_options)
+
+        sigmas = comfy.samplers.calculate_sigmas(real_model, steps + 1, scheduler, sampler_name).flip(0).to(device)[1:]
+
+        samples = sampler.sample(noise, positive_copy, negative_copy, cfg=cfg, latent_image=latent_image, force_full_denoise=False, denoise_mask=noise_mask, sigmas=sigmas)
+        samples = samples.cpu()
+        
+        comfy.sample.cleanup_additional_models(models)
+
+        out = latent.copy()
+        out["samples"] = samples
+        return (out, )
+    
+NODE_CLASS_MAPPINGS = {
+    "NoisyLatentImage": NoisyLatentImage,
+    "DuplicateBatchIndex": DuplicateBatchIndex,
+    "LatentSlerp": LatentSlerp,
+    "GetSigma": GetSigma,
+    "InjectNoise": InjectNoise,
+    "Unsampler": Unsampler,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "NoisyLatentImage": "Noisy Latent Image",
+    "DuplicateBatchIndex": "Duplicate Batch Index",
+    "LatentSlerp": "Slerp Latents",
+    "GetSigma": "Get Sigma",
+    "InjectNoise": "Inject Noise",
+    "Unsampler": "Unsampler",
+}
