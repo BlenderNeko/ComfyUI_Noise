103524d2efe3a7457b379ea2c80b7d9dc8713942
Dr.Lt.Data <dr.lt.data@gmail.com>
2024-04-09 00:17:38 +0900

    unsampler - compatibility patch
    https://github.com/comfyanonymous/ComfyUI/commit/0542088ef895b4825df80fd3babf91513441af65

--- nodes.py
+++ nodes.py
@@ -7,6 +7,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "co
 
 import comfy.model_management
 import comfy.sample
+import comfy.sampler_helpers
 
 MAX_RESOLUTION=8192
 
@@ -210,24 +211,26 @@ class Unsampler:
         noise = torch.zeros(latent_image.size(), dtype=latent_image.dtype, layout=latent_image.layout, device="cpu")
         noise_mask = None
         if "noise_mask" in latent:
-            noise_mask = comfy.sample.prepare_mask(latent["noise_mask"], noise.shape, device)
-
-        real_model = None
-        real_model = model.model
+            noise_mask = comfy.sampler_helpers.prepare_mask(latent["noise_mask"], noise.shape, device)
 
         noise = noise.to(device)
         latent_image = latent_image.to(device)
 
-        positive = comfy.sample.convert_cond(positive)
-        negative = comfy.sample.convert_cond(negative)
+        conds0 = \
+            {"positive": comfy.sampler_helpers.convert_cond(positive),
+             "negative": comfy.sampler_helpers.convert_cond(negative)}
 
-        models, inference_memory = comfy.sample.get_additional_models(positive, negative, model.model_dtype())
+        conds = {}
+        for k in conds0:
+            conds[k] = list(map(lambda a: a.copy(), conds0[k]))
+
+        models, inference_memory = comfy.sampler_helpers.get_additional_models(conds, model.model_dtype())
         
         comfy.model_management.load_models_gpu([model] + models, model.memory_required(noise.shape) + inference_memory)
 
-        sampler = comfy.samplers.KSampler(real_model, steps=steps, device=device, sampler=sampler_name, scheduler=scheduler, denoise=1.0, model_options=model.model_options)
+        sampler = comfy.samplers.KSampler(model, steps=steps, device=device, sampler=sampler_name, scheduler=scheduler, denoise=1.0, model_options=model.model_options)
 
-        sigmas = sigmas = sampler.sigmas.flip(0) + 0.0001
+        sigmas = sampler.sigmas.flip(0) + 0.0001
 
         pbar = comfy.utils.ProgressBar(steps)
         def callback(step, x0, x, total_steps):
@@ -240,7 +243,7 @@ class Unsampler:
             samples /= samples.std()
         samples = samples.cpu()
         
-        comfy.sample.cleanup_additional_models(models)
+        comfy.sampler_helpers.cleanup_additional_models(models)
 
         out = latent.copy()
         out["samples"] = samples
